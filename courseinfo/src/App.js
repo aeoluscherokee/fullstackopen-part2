c18b62b9b1c0ea5a90fe8521f85f0fd6b8a88707
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-03 23:25:05 +0700

    Import Course component to App component

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,52 +1,5 @@
 import React from "react";
-
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  );
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <div>
-      {props.parts.map((part, id) => (
-        <Part key={id} name={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-};
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((s, p) => {
-    return s + p.exercises;
-  }, 0);
-
-  return (
-    <p>
-      <strong>total of {total} exercises</strong>
-    </p>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
+import Course from "./Course";
 
 const App = () => {
   const courses = [

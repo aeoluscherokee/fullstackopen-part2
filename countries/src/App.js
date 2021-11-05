63338c9c779d1ed15320f0382d6ee43798fc68bb
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-05 16:20:25 +0700

    Fetch countries using axios

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,10 +1,23 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [allCountries, setAllCountries] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => setAllCountries(response.data));
+  }, []);
+  console.log(allCountries);
   return (
     <div>
       <div>
         find countries <input />
+        <div>
+          {allCountries.map((country) => (
+            <p>{country.name.common}</p>
+          ))}
+        </div>
       </div>
     </div>
   );

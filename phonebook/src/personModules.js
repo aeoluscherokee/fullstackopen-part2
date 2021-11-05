b31bb6b7bb19654a2214e02acd88252231015ed7
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-06 01:15:57 +0700

    Add update module

--- phonebook/src/personModules.js
+++ phonebook/src/personModules.js
@@ -12,8 +12,13 @@ export const create = (newObject) => {
 };
 
 export const del = (id) => {
-  const request = axios.delete(baseUrl + "/" + id);
-  return request.then(() => getAll());
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
+
+export const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((response) => response.data);
 };
 
 export default getAll;

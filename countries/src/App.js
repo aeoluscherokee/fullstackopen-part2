f78d4f38312f53cc480a5420af7aeb7b278200e8
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-05 16:50:40 +0700

    Filter countries with search field

--- countries/src/App.js
+++ countries/src/App.js
@@ -3,19 +3,30 @@ import axios from "axios";
 
 function App() {
   const [allCountries, setAllCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
   useEffect(() => {
     axios
       .get("https://restcountries.com/v3.1/all")
       .then((response) => setAllCountries(response.data));
   }, []);
-  console.log(allCountries);
+  const handleOnSearchChange = (event) => {
+    event.preventDefault();
+    setFilteredCountries(
+      allCountries.filter((country) =>
+        country.name.common
+          .toLowerCase()
+          .includes(event.target.value.toLowerCase())
+      )
+    );
+  };
+
   return (
     <div>
       <div>
-        find countries <input />
+        find countries <input onChange={handleOnSearchChange} />
         <div>
-          {allCountries.map((country) => (
-            <p>{country.name.common}</p>
+          {filteredCountries.map((country, id) => (
+            <p key={id}>{country.name.common}</p>
           ))}
         </div>
       </div>

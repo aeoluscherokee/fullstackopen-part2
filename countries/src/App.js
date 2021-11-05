84f3717de63ef3214b2733acf914cbaaea25d66e
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-05 17:37:39 +0700

    Create Country component to render single country

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,17 +1,33 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+const Country = ({ country }) => {
+  console.log(country);
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <p>capital {country.capital.map((el) => el)}</p>
+      <p>{country.population}</p>
+      <h2>language</h2>
+      <ul>
+        {Object.values(country.languages).map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
+      <img alt="flag" src={country.flags.png}></img>
+    </div>
+  );
+};
+
 const Countries = ({ filteredCountries }) => {
-  if (filteredCountries.length <= 10) {
-    return filteredCountries.map((country, id) => (
-      <p key={id}>{country.name.common}</p>
-    ));
-  } else if (filteredCountries === 1) {
-    return filteredCountries.map((country, id) => (
-      <p key={id}>{country.name.common}</p>
-    ));
+  if (filteredCountries.length === 1) {
+    return <Country country={filteredCountries[0]} />;
   } else if (filteredCountries.length === 0) {
     return null;
+  } else if (filteredCountries.length <= 10) {
+    return filteredCountries.map((country, id) => (
+      <p key={id}>{country.name.common}</p>
+    ));
   } else {
     return <p>"Too many matches, specify another filter</p>;
   }

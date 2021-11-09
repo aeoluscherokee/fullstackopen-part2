a8417f9ca8ce1813b52ba3c8715f8dbe3aac8cd4
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-09 20:11:20 +0700

    Prettify code

--- phonebook/src/components/Notification.js
+++ phonebook/src/components/Notification.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
 const Notification = ({ notification }) => {
   if (notification) {
     return <h2 className={notification.type}>{notification.message}</h2>;
   } else return null;
 };
-export default Notification
+export default Notification;
